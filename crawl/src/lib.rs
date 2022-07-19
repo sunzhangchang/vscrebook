156dd752719dae435a02446ef5227b54812ca722
sunzhangchang <642311409@qq.com>
2022-07-19 09:37:01 +0000

    t

--- crawl/src/lib.rs
+++ crawl/src/lib.rs
@@ -1,15 +1,29 @@
-use std::collections::HashMap;
+use std::fmt::Debug;
 
-use async_trait::async_trait;
-use reqwest::Result;
-use serde::Deserialize;
+// use async_trait::async_trait;
+use js_sys::{Object, Array, Reflect};
 use wasm_bindgen::prelude::*;
 
-#[derive(Deserialize)]
-struct ShowMoreInfo {
-    caimoge: bool,
-    wbxsw: bool,
-    aixiashu: bool,
+// #[derive(Default)]
+// struct ShowMoreInfo {
+//     caimoge: bool,
+//     wbxsw: bool,
+//     aixiashu: bool,
+// }
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(extends = Object, typescript_type = "ShowMoreInfo")]
+    type ShowMoreInfo;
+
+    #[wasm_bindgen(constructor)]
+    fn dft() -> ShowMoreInfo;
+}
+
+impl Default for ShowMoreInfo {
+    fn default() -> Self {
+        Self::dft()
+    }
 }
 
 enum DownSet {
@@ -19,23 +33,69 @@ enum DownSet {
     TxtAndChapters,
 }
 
-type DownloadSettings = HashMap<String, DownSet>;
+// #[derive(Default)]
+// struct DownloadSettings {
+//     caimoge: String,
+//     wbxsw: String,
+//     aixiashu: String,
+// }
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(extends = Object, typescript_type = "DownloadSettings")]
+    type DownloadSettings;
+
+    #[wasm_bindgen(constructor)]
+    fn dft() -> DownloadSettings;
+}
+
+impl Default for DownloadSettings {
+    fn default() -> Self {
+        Self::dft()
+    }
+}
+
+// type DownloadSettings = HashMap<String, String>;
 
 type DownThreadAmount = u16;
 
-#[derive(Deserialize)]
+#[derive(Default)]
 struct Config {
     show_more_info: ShowMoreInfo,
     download_settings: DownloadSettings,
     down_thread_amount: DownThreadAmount,
 }
 
-fn to_config(s: &mut String) -> Config {
-}
-
 #[wasm_bindgen]
 extern "C" {
     fn get_config() -> JsValue;
+}
+
+fn g_config() -> Config {
+    let js_configs = unsafe { get_config() };
+    let js_download_settings = unsafe { Reflect::get(&js_configs, &"downloadSettings".into()).unwrap() };
+    let mut config = Config::default();
+    if js_download_settings.is_object() {
+        let t: Object = js_download_settings.into();
+        for i in Array::iter(&Object::entries(&t)) {
+            let a: Array = i.into();
+            let key: &str = &a.at(0).as_string().unwrap();
+            let value: JsValue = a.at(1);
+            match key {
+                "showMoreInfo" => {
+                    config.show_more_info = value.into();
+                }
+                "downThreadAmount" => {
+                    config.down_thread_amount = value.as_f64().unwrap() as u16
+                }
+                "downloadSettings" => {
+                    config.download_settings = value.into();
+                }
+                &_ => {}
+            };
+        }
+    }
+    config
 }
 
 // declare type SearchBook = {
@@ -52,41 +112,81 @@ extern "C" {
 // }
 
 #[wasm_bindgen]
-struct search_book {
+#[derive(Debug)]
+pub struct SearchBook {
+    // 书名: String,
+    // 作者: String,
+    // 状态: String,
+    // 分类: String,
+    // 字数: String,
+    // 简介: String,
+    // 最新章节: String,
+    // 最近更新: String,
+    // 目录链接: String,
+    // 书源: Source,
 }
 
-#[async_trait]
+// #[async_trait]
 trait Crawl {
-    async fn search(search_key: &str) -> Result<Vec<search_book>>;
+    fn search(&self, search_key: &str) -> Result<Vec<SearchBook>, String>;
+    // async fn search(&self, search_key: &str) -> Result<Vec<SearchBook>, String>;
+}
+
+struct CrawlD {
+    source_name: String,
 }
 
 struct ACrawl {
+    info: CrawlD,
 }
 
-#[async_trait]
-impl Crawl for ACrawl {
-    async fn search(&mut self, search_key: &str) -> Result<Vec<search_book>> {
-        unsafe {
-            let configs = get_config().to_config();
-        }
-        if (_.isEqual(getConfig().downloadSettings[this.sourceName], 'disable')) {
-            return []
-        }
-        return this.searchDetail(searchKey)
-        Ok(list)
+impl ACrawl {
+    fn search_detail(&self, search_key: &str) -> Vec<SearchBook> {
+        vec![]
     }
+}
+
+// #[async_trait]
+impl Crawl for ACrawl {
+    fn search(&self, search_key: &str) -> Result<Vec<SearchBook>, String> {
+        let configs = g_config();
+        let source_name = &((&self.info).source_name);
+        if unsafe{Reflect::get(&configs.download_settings, &source_name.into())}.unwrap().as_string().unwrap() == "disable" {
+            return Ok(vec![]);
+        }
+        Ok(self.search_detail(search_key))
+    }
+    // async fn search(&self, search_key: &str) -> Result<Vec<SearchBook>, String> {
+    //     let configs = g_config();
+    //     let source_name = &((&self.info).source_name);
+    //     if unsafe{Reflect::get(&configs.download_settings, &source_name.into())}.unwrap().as_string().unwrap() == "disable" {
+    //         return Ok(vec![]);
+    //     }
+    //     Ok(self.search_detail(search_key))
+    // }
 }
 
 struct Caimoge {
 }
 
-
 #[wasm_bindgen]
-pub async fn search(search_key: &str) -> Result<Vec<search_book>> {
+pub fn search(search_key: String) -> String {
     let mut list = Vec::new();
-    let crawlers = [];
-    for i in crawlers {
-        list.append(i.search(search_key).await?)
-    }
-    Ok(list)
+    let c = ACrawl {
+        info: CrawlD {
+            source_name: "采墨阁".to_string(),
+        }
+    };
+    list.append(&mut c.search(&search_key).unwrap());
+    format!("{:?}", list)
 }
+// pub async fn search(search_key: String) -> String {
+//     let mut list = Vec::new();
+//     let c = ACrawl {
+//         info: CrawlD {
+//             source_name: "采墨阁".to_string(),
+//         }
+//     };
+//     list.append(&mut c.search(&search_key).await.unwrap());
+//     format!("{:?}", list)
+// }
