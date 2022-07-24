3b546ccdaae34a5152f14e720607cbf718fcd89d
sunzhangchang <642311409@qq.com>
2022-07-24 11:26:07 +0000

    download

--- crawl/src/lib.rs
+++ crawl/src/lib.rs
@@ -3,19 +3,21 @@ mod utils;
 mod crawlers;
 mod search_book;
 
+use std::{path::PathBuf, io::Write};
+
 use crawlers::wbxsw::Wbxsw;
 use crawlers::crawl::Crawl;
 use crawlers::aixiashu::Aixiashu;
 use crawlers::caimoge::Caimoge;
-use futures::join;
+use futures::future::join_all;
 use js_sys::{Object, Reflect};
 use search_book::SearchBook;
 use wasm_bindgen::prelude::*;
 
 use crate::crawlers::maxreader::Maxreader;
 
-#[wasm_bindgen]
-pub async fn search(search_key: String) -> Object {
+#[wasm_bindgen(js_name = rsSearch)]
+pub async fn rs_search(search_key: String) -> Object {
     let mut results: Vec<SearchBook> = Vec::new();
     let mut errors: Vec<String> = Vec::new();
 
@@ -25,12 +27,12 @@ pub async fn search(search_key: String) -> Object {
         }
     }
 
-    let res = join!(
+    let res = join_all([
         s!(Aixiashu{}),
         s!(Caimoge{}),
         s!(Wbxsw{}),
         s!(Maxreader{}),
-    );
+    ]).await;
 
     macro_rules! push_res {
         ($($x: expr), *) => {
@@ -43,10 +45,46 @@ pub async fn search(search_key: String) -> Object {
         };
     }
     
-    push_res!(res.0, res.1, res.2, res.3);
+    for i in res {
+        push_res!(i);
+    }
+    // push_res!(res.0, res.1, res.2, res.3);
 
     let obj = Object::default();
     Reflect::set(&obj, &"results".into(), &serde_json::to_string(&results).unwrap().into()).unwrap();
     Reflect::set(&obj, &"errors".into(), &serde_json::to_string(&errors).unwrap().into()).unwrap();
     obj
+}
+
+#[wasm_bindgen(js_name = rsDownload)]
+pub async fn rs_download(source: String, menu_url: String, dir: String, name: String) -> Result<String, String> {
+    let result = match source.as_str() {
+        Aixiashu::SOURCE_NAME => Aixiashu{}.download(&menu_url),
+        Caimoge::SOURCE_NAME => Caimoge{}.download(&menu_url),
+        Wbxsw::SOURCE_NAME => Wbxsw{}.download(&menu_url),
+        Maxreader::SOURCE_NAME => Maxreader{}.download(&menu_url),
+        _ => unreachable!(),
+    }.await;
+
+    match result {
+        Ok(data) => {
+            let mut buffer = PathBuf::from(&dir);
+            buffer.push(&name);
+            buffer.set_extension("txt");
+    
+            let path = buffer.as_path();
+    
+            std::fs::create_dir_all(&dir).unwrap();
+    
+            let mut f = std::fs::File::create(path).unwrap();
+            f.write(data.as_bytes()).unwrap();
+    
+            if let Some(path) = path.as_os_str().to_str() {
+                Ok(path.to_string())
+            } else {
+                Err("The download path is not a valid UTF-8 string".to_string())
+            }
+        }
+        Err(e) => Err(e)
+    }
 }
--- src/crawl/index.ts
+++ src/crawl/index.ts
@@ -11,7 +11,7 @@ import { setExtTo } from "../utils"
 import { Wbxsw } from "./sub/wbxsw"
 import { Aixiashu } from "./sub/aixiashu"
 import axiosRetry from "axios-retry"
-import { search as rsSearch } from "../../crawl/pkg/crawl"
+import { rsDownload, rsSearch } from "../../crawl/pkg/crawl"
 
 axiosRetry(axios, {
     retries: 3,
@@ -26,47 +26,57 @@ axios.defaults.timeout = 3000
 axios.defaults.headers.common['User-Agent'] = USER_AGENT
 axios.defaults.responseType = 'arraybuffer'
 
-const crawlers: Crawl[] = [
-    new Aixiashu(),
-    new Wbxsw(),
-    new Caimoge(),
-]
+// const crawlers: Crawl[] = [
+//     new Aixiashu(),
+//     new Wbxsw(),
+//     new Caimoge(),
+// ]
 
 export async function search(searchKey: string): Promise<SearchBook[]> {
-    const res = (await rsSearch(searchKey)) as {
-        results: string
-        errors: string
+    try {
+        const res = (await rsSearch(searchKey)) as {
+            results: string
+            errors: string
+        }
+        const errors = JSON.parse(res.errors) as string[]
+        console.error(errors)
+        errors.forEach(e => window.showErrorMessage(e))
+        return JSON.parse(res.results)
+    } catch(e) {
+        console.error(e)
+        window.showErrorMessage((e as Error).message)
+        throw e
     }
-    const errors = JSON.parse(res.errors) as string[]
-    console.error(errors)
-    errors.forEach(e => window.showErrorMessage(e))
-    return JSON.parse(res.results)
 }
 
 export async function download(source: string, menuURL: string, dir: string, name: string): Promise<string> {
-    const spider = crawlers.find(iter => _.isEqual(iter.sourceName, source))
-
-    if (_.isUndefined(spider)) {
-        myerror(Errors.downloadNovelFailed)
-        throw new Error(`${source} cannot find crawl!`)
-    }
-
-    const data = await spider.download(menuURL)
-
-    if (_.isNull(data)) {
-        myerror(Errors.downloadNovelFailed)
-        throw new Error(`${menuURL} cannot fetch anything!`)
-    }
-
-    return (() => {
-        const pth = join(dir, setExtTo(parse(name).name, 'txt'))
-        writeFileSync(pth, data, {
-            encoding: "utf8"
-        })
-        window.showInformationMessage('下载完成!')
-        return pth
-    })()
+    return rsDownload(source, menuURL, dir, name)
 }
+
+// export async function download(source: string, menuURL: string, dir: string, name: string): Promise<string> {
+//     const spider = crawlers.find(iter => _.isEqual(iter.sourceName, source))
+
+//     if (_.isUndefined(spider)) {
+//         myerror(Errors.downloadNovelFailed)
+//         throw new Error(`${source} cannot find crawl!`)
+//     }
+
+//     const data = await spider.download(menuURL)
+
+//     if (_.isNull(data)) {
+//         myerror(Errors.downloadNovelFailed)
+//         throw new Error(`${menuURL} cannot fetch anything!`)
+//     }
+
+//     return (() => {
+//         const pth = join(dir, setExtTo(parse(name).name, 'txt'))
+//         writeFileSync(pth, data, {
+//             encoding: "utf8"
+//         })
+//         window.showInformationMessage('下载完成!')
+//         return pth
+//     })()
+// }
 
 // // eslint-disable-next-line no-useless-escape
 // const indexReg = /PART\b|^Prologue|Chapter\s*[-_]?\d+|分卷|^序$|^序\s*言|^序\s*章|^前\s*言|^附\s*[录錄]|^引\s*[言子]|^摘\s*要|^[楔契]\s*子|^后\s*记|^後\s*記|^附\s*言|^结\s*语|^結\s*語|^尾\s*[声聲]|^最終話|^最终话|^番\s*外|^\d+\s*\D*[^\d#\.]$|^[第（]?[\d〇零一二三四五六七八九十百千万萬-]+\s*[、）章节節回卷折篇幕集话話]/i
