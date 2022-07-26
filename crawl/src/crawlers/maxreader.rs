0956fa79dc81c8dd2805d5dfe8120e353224b6d6
sunzhangchang <642311409@qq.com>
2022-07-26 01:58:56 +0000

    fix(crawler): :bug: 修复醉读下载

--- crawl/src/crawlers/maxreader.rs
+++ crawl/src/crawlers/maxreader.rs
@@ -28,7 +28,7 @@ impl Crawl for Maxreader {
             let menu_url = i.select("a:nth-child(1)").attr("href");
             if let Some(url) = menu_url {
                 let url: String = url.into();
-                let url = url.replace("read", "book");
+                let url = url.replace("book", "read");
                 if let Ok(u) = Url::parse(Self::SEARCH_URL) {
                     if let Ok(menu_url) = u.join(&url) {
                         let mut status = "未知".to_string();
